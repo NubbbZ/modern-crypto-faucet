505fdc5e70a8a977458415d6712a366f5fa65f78
NubbbZ <4251461+NubbbZ@users.noreply.github.com>
2025-08-22 16:14:51 -0400

    Added try catch for the wallets functions and a wallet connection check.

--- app/Wallet.php
+++ app/Wallet.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Sajya\Client\Client;
 
 class Wallet
@@ -17,28 +18,59 @@ class Wallet
 
         $url = Http::baseUrl('http://' . $username . ':' . $password . '@' . $host);
         $this->client = new Client($url); //https://user:pass@127.0.0.1:8332
+
+        $this->testConnection();
     }
 
     public function validateAddress($address): bool
     {
-        $response = $this->client->execute('validateaddress', [$address]);
-        if (isset($response->result()['isvalid']) && $response->result()['isvalid']) {
-            return true;
+        $this->testConnection();
+
+        try {
+            $response = $this->client->execute('validateaddress', [$address]);
+            if (isset($response->result()['isvalid']) && $response->result()['isvalid']) {
+                return true;
+            }
+            return false;
+        } catch (\Exception $e) {
+            Log::error('Error validating address!', ['message' => $e->getMessage()]);
+            return false;
         }
-        return false;
     }
 
-    public function getBalance()
+    public function getBalance(): ?int
     {
-        $response = $this->client->execute('getbalance');
-        if ($response->result() == null) return 0;
-        return $response->result();
+        $this->testConnection();
+
+        try {
+            return $this->client->execute('getbalance')->result();
+        } catch (\Exception $e) {
+            Log::error('Error fetching balance!', ['message' => $e->getMessage()]);
+            return null;
+        }
     }
 
     public function sendToAddress($address, $amount)
     {
-        $response = $this->client->execute('sendtoaddress', [$address, $amount]);
-        //return $response->result();
+        $this->testConnection();
+
+        try {
+            $response = $this->client->execute('sendtoaddress', [$address, $amount]);
+            return $response->result();
+        } catch (\Exception $e) {
+            Log::error('Error sending to address!', ['message' => $e->getMessage()]);
+            return null;
+        }
+    }
+
+    private function testConnection()
+    {
+        try {
+            $this->client->execute('getblockchaininfo');
+        } catch (\Exception $e) {
+            Log::error('Wallet connection error!', ['message' => $e->getMessage()]);
+            abort(503, 'Wallet connection error!');
+        }
     }
 }
 
